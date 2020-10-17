9343a3f2ca469c193bf7bf96be356aa1335caeb8
KenFai <ngkenfai@gmail.com>
2020-10-17 14:52:01 +0800

    Initial Commit

---
+++ WidgetB/WidgetBApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetBApp.swift
+//  WidgetB
+//
+//  Created by Ginger on 17/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct WidgetBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
